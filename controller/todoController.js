aca589651f8ad621de59a8484dcc571465ef9fc9
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-22 22:56:13 +0600

    update todo done

--- controller/todoController.js
+++ controller/todoController.js
@@ -30,4 +30,22 @@ export const getSingleTodo = async (req, res) => {
     return res.status(500).json(error.message);
   }
 };
- 
+
+export const updateTodo = async (req, res) => {
+  console.log(`You are trying to update todo with id ${req.params.id}`)
+  console.log(req.body.todo)
+
+  try {
+    const update = await todo.findOneAndUpdate(
+      { _id: req.params.id },
+      { data: req.body.todo }
+    );
+
+    await update.save();
+    return res.status(200).json(update);
+  } 
+  
+  catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addTodo,getAllTodos,getSingleTodo } from "../controller/todoController.js";
+import { addTodo,getAllTodos,getSingleTodo,updateTodo } from "../controller/todoController.js";
 const route = express.Router();
 route.post("/todos", addTodo);
 route.get("/todos", getAllTodos);
 route.get("/todos/:id", getSingleTodo);
+route.put("/todos/:id", updateTodo);
+
 
 
 
